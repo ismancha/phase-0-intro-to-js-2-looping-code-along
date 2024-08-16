3b34d1b311e4676c67e4bf8cd036fa346af52bea
Isaiah <mancha.isaiah@gmail.com>
2024-08-16 14:27:39 -0400

    looping code lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, gift) {
+  let messages = [];
+
+  names.forEach(name => {
+    let message = `Thank you, ${name}, for the wonderful ${gift} gift!`;
+    messages.push(message);
+  });
+
+  return messages;
+}
+function countDown(number) {
+    if (typeof number !== 'number' || number <= 0) {
+      throw new Error('Invalid input. Please provide a positive integer greater than zero.');
+    }
+  
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
